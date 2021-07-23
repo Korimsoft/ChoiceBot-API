768240b36aacb1377b0cd04bd835f7cea82d45e2
Tomas Korima <tomas.korima@certicon.cz>
2021-07-23 14:48:44 +0200

    Hello world buildable and runnable with npm.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
-function main() {
+import express from 'express';
 
+
+function main() {
+    const app = express();
+    const port = 8088;
+
+    app.get('/', (req, res) => {
+        res.status(200).send('Hello kundy \n');
+    });
+
+    app.listen(port, () => {
+        console.log(`Listening on ${port}.`);
+    })
 }
 
 main();
